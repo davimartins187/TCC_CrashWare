4b1ac7ab0208bea7679a70c739a94214300d2496
FelipeBarbosa044 <felipebarbosaribeiro197@gmail.com>
2026-04-15 20:31:16 -0300

    Verificando email
    Criei um afunção em que manda o codigo enviado pelo email

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -41,33 +41,29 @@ def gerar_codigo():
     codigo = str(randint(100000,999999))
     expira_em = datetime.now(timezone.utc) + timedelta(minutes=10)
 
-
     return codigo , expira_em
-
+######
 
 def enviar_email(codigo, destinario):
-
-
-	#Aqui vou colocar a mensagem que eu quero enviar.(A MENSAGEM TEM QUE ESTAR EM HTML)
-
-	corpo_email = f"""
-	<p>Olá,<p>
-	<p>Nós da CrashWare recebemos uma solicitação para verificar o seu e-mail em nossa plataforma.</p>
-	<p>Para continuar, utilize o código de verificação abaixo:/<p>
-	<h1>Código: {codigo} </h1>
-	<p>Este código é válido por 10 minutos.</p>
-	<p>Se você não fez essa solicitação, ignore este e-mail.</p>
-	<p>Atenciosamente,</p>
+    #Aqui vou colocar a mensagem que eu quero enviar.(A MENSAGEM TEM QUE ESTAR EM HTML)
+    corpo_email =  f"""
+    <p>Olá,</p>
+    <p>Nós da CrashWare recebemos uma solicitação para verificar o seu e-mail em nossa plataforma.</p>
+    <p>Para continuar, utilize o código de verificação abaixo:</p>
+    <h1>Código: {codigo} </h1>
+    <p>Este código é válido por 10 minutos.</p>
+    <p>Se você não fez essa solicitação, ignore este e-mail.</p>
+    <p>Atenciosamente,</p>
     <p>Equipe CrashWare</p>
-	"""
+    """
 
-    #Enviando código para o email
+
     msg = email.message.Message()
     msg['Subject'] = "Verificação de e-mail - CrashWare"  # Assunto/Titulo do email
     msg['From'] = 'plataformacrashware@gmail.com'  #que vai enviar a mensagem
     msg['To'] = f'{destinario}'  # Email que vai receber a mensagem
     password = f'{PASSWORD_EMAIL}'  # Senha do remetente.
-    msg.add_header('Content=Type', 'text/html')
+    msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email)
 
     s = smtplib.SMTP('smtp.gmail.com: 587')
@@ -75,7 +71,7 @@ def enviar_email(codigo, destinario):
     # Login Credentials for sending the mail
     s.login(msg['From'], password)
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-
+    print('Email enviado')
 
 
 
@@ -88,13 +84,16 @@ async def cadastro(dados : UsuarioSchema,session = Depends(pegar_sessao)):
     else:
         codigo , expira = gerar_codigo()
         try:
+
+            # Enviando codigo para o email
+            enviar_email(codigo, dados.email)
+
             senha_criptografada = criptografia.hash(dados.senha)
             usuario_novo = Usuarios(nome_usuario =dados.nome_usuario.title(), email= dados.email , senha_hash= senha_criptografada,codigo = codigo, codigo_expirado_em = expira)
             session.add(usuario_novo)
             session.commit()
 
-            #Enviando codigo para o email
-            enviar_email(codigo, dados.email)
+
 
             #Resposta da API
             return{
