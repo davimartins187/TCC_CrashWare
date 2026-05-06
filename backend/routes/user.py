ff794c91de26eb6dbd302a2a5c5245460993eea5
FelipeBarbosa044 <felipebarbosaribeiro197@gmail.com>
2026-05-06 10:35:15 -0300

    Importando Patente e não Nivel

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends,HTTPException
 #Importando tabelas:
 from models.usuarios import Usuarios
 from models.usuarios_oauth import UsuariosOauth
-from models.patente import Nivel
+from models.patente import Patente
 
 
 #Instânciando roteador
